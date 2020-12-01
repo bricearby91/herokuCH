bec61607272246dd77a2ab8e6fc808c7ccdb33e4
Arby Brice <brice.arby91@gmail.com>
2020-12-01 14:11:20 +0100

    committing routes.rb + team

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-  
-  get 'orders/index'
-  root 'products#index'
-  
   devise_for :users
-	resources :contact, only: [:index]
-  resources :products
-	resources :team, only: [:index]
-
-  resources :users, only: [:show] do
-    resources :carts, only: [:show] do
-      resources :charges, only: [:new, :create]
-    end
-  end
+  root 'posts#index'
+  resources :posts
+  resources :team, only: [:index] 
+end
